2304e1a846174ff9b7595cff64148e884c076b50
javiergranadosr <jgrinformatic@gmail.com>
2020-05-23 17:25:08 -0500

    Deploy app in Heorku

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,27 +5,27 @@
         <p class="mt-2"><span class="badge badge-dark">POST</span> REQUESTS</p>
         <p>
           Crear cliente
-          <code>POST || https://api-store-php.herokuapp.com/client</code>
+          <code>POST || url/client</code>
         </p>
         <p>
           Crear categoría
-          <code>POST || https://api-store-php.herokuapp.com/category</code>
+          <code>POST || url/category</code>
         </p>
         <p>
           Crear producto
-          <code>POST || https://api-store-php.herokuapp.com/product</code>
+          <code>POST || url/product</code>
         </p>
         <p class="mt-2"><span class="badge badge-dark">PUT</span> REQUESTS</p>
         <p>
           Actualizar categoría
           <code
-            >PUT || https://api-store-php.herokuapp.com/update-category/id</code
+            >PUT || url/update-category/id</code
           >
         </p>
         <p>
           Actualizar producto
           <code
-            >PUT || https://api-store-php.herokuapp.com/update-product/id</code
+            >PUT || url/update-product/id</code
           >
         </p>
         <p class="mt-2">
@@ -35,14 +35,14 @@
           Eliminar categoría
           <code
             >DELETE ||
-            https://api-store-php.herokuapp.com/delete-category/id</code
+            url/delete-category/id</code
           >
         </p>
         <p>
           Eliminar producto
           <code
             >DELETE ||
-            https://api-store-php.herokuapp.com/delete-product/id</code
+            url/delete-product/id</code
           >
         </p>
       </b-col>
@@ -50,19 +50,19 @@
         <p class="mt-2"><span class="badge badge-dark">GET</span> REQUESTS</p>
         <p>
           Listar categorías
-          <code>GET || https://api-store-php.herokuapp.com/categories</code>
+          <code>GET || url/categories</code>
         </p>
         <p>
           Listar categoría por id
-          <code>GET || https://api-store-php.herokuapp.com/category/id</code>
+          <code>GET || url/category/id</code>
         </p>
         <p>
           Listar productos
-          <code>GET || https://api-store-php.herokuapp.com/products</code>
+          <code>GET || url/products</code>
         </p>
         <p>
           Listar producto por id
-          <code>GET || https://api-store-php.herokuapp.com/product/id</code>
+          <code>GET || url/product/id</code>
         </p>
       </b-col>
     </b-row>
